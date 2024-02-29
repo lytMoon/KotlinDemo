ad149dc387a9527c7034f6bc0003d6cf37241cfa
lytMoon <2936437824@qq.com>
2024-02-29 20:55:16 +0800

    :fire: 学习阶段1

--- Coroutine/app/src/main/java/com/example/coroutinedemo/MainActivity.kt
+++ Coroutine/app/src/main/java/com/example/coroutinedemo/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.coroutinedemo
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
@@ -13,31 +18,58 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mTv: TextView
+
+    @SuppressLint("MissingInflatedId")
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /**
-         * 开启协程的三种方法
-         */
+        mTv = findViewById(R.id.tv_name)
 
-        //返回的是最后一行代码
-        runBlocking {
-
-        }
-        //返回一个Job
-        GlobalScope.launch {
-
-        }
-        //返回Deferred对象
-        GlobalScope.launch {
-
-        }
 
 //        start()
 //        start1()
+//        start3()
+//        start4()
+//        testDefaultStart()
+//        testLazyStart()
+//        testAtomicStart()
+        testCoroutineStart()
 
 
+    }
+
+    /**
+     *
+     */
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun start4() {
+        GlobalScope.launch {
+            start5()
+        }
+    }
+
+    private suspend fun start5() {
+
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun start3() {
+        /**
+         * 这里我们新开了一个协程。挂起在IO线程。
+         */
+        GlobalScope.launch(Dispatchers.Main) {
+            withContext(Dispatchers.IO) {
+                Log.d("LogTest", "测试数据:${Thread.currentThread()}")
+                val a = 5 + 7
+//                Toast.makeText(this@MainActivity,Thread.currentThread().toString(),Toast.LENGTH_SHORT).show()
+//                mTv.text=Thread.currentThread().toString()
+            }
+            //这里的UI刷新成功，显然此刻为主线程。
+            mTv.text = Thread.currentThread().toString()
+            Log.d("LogTest", "测试数据:${Thread.currentThread()}")
+        }
     }
 
     @OptIn(DelicateCoroutinesApi::class)
@@ -60,12 +92,12 @@ class MainActivity : AppCompatActivity() {
     @OptIn(DelicateCoroutinesApi::class)
     private fun start1() {
         GlobalScope.launch(Dispatchers.Main) {
-//            for (index in 1 until 100) {
-//                //同步执行
-//                launch {
-//                    Log.d("launch it", "${index}:启动一个协程")
-//                }
-//            }
+            for (index in 1 until 100) {
+                //同步执行
+                launch {
+                    Log.d("launch it", "${index}:启动一个协程")
+                }
+            }
 
             for (index in 1 until 1000) {
                 if (index in 200 until 600) {
@@ -99,6 +131,78 @@ class MainActivity : AppCompatActivity() {
     suspend fun doSomethingUsefulTwo(): Int {
         delay(1000L) // 假设我们在这里也做了一些有用的事
         return 29
+    }
+
+    /**
+     * 一般情况下为饿汉模式启动，不过有可能被取消掉。
+     * 先开启一个协程调度到IO再回来。回来之后打印当前的Main线程，接着执行join
+     */
+
+
+    /**
+     * 这里打印的Log没有测试数据2.也就是第一个挂起函数被取消了，但是这两个不都是运行在主线程的吗？
+     * 因为协程是不会阻塞线程的，不管调度的线程是否和原线程一样。所以不会先执行上面，开启第一个线程的时候第二个已经执行了。
+     */
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun testDefaultStart() {
+        GlobalScope.launch(Dispatchers.Main) {
+            val job = launch(Dispatchers.Main) {
+                Log.d("LogTest", "测试数据:2")
+                Log.d("LogTest", "测试数据:${Thread.currentThread()}")
+            }
+            Log.d("LogTest", "测试数据:${Thread.currentThread()}")
+            job.cancel()
+        }
+    }
+
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun testLazyStart() {
+        GlobalScope.launch {
+            val lazy = launch(start = CoroutineStart.LAZY) {
+                Log.d("LogTest", "测试数据:${"我是LAZY启动模式"}")
+            }
+            Log.d("LogTest", "测试数据:执行1")
+            lazy.join()
+        }
+    }
+
+
+    @OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
+    private fun testAtomicStart() {
+        GlobalScope.launch(Dispatchers.Main) {
+            val job = launch(Dispatchers.Main, start = CoroutineStart.ATOMIC) {
+                Log.d("LogTest", "测试数据:2")
+//                delay(100)
+                Log.d("LogTest", "测试数据:${Thread.currentThread()}")
+            }
+            job.cancel()
+            Log.d("LogTest", "测试数据:${Thread.currentThread()}")
+        }
+    }
+
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun testCoroutineStart() {
+        val defaultJob = GlobalScope.launch {
+            Log.d("LogTest", "CoroutineStart.DEFAULT")
+        }
+        defaultJob.cancel()
+        val lazyJob = GlobalScope.launch(start = CoroutineStart.LAZY) {
+            Log.d("LogTest", "CoroutineStart.LAZY")
+        }
+        val atomicJob = GlobalScope.launch(start = CoroutineStart.ATOMIC) {
+            Log.d("LogTest", "CoroutineStart.ATOMIC挂起前")
+            delay(100)
+            Log.d("LogTest", "CoroutineStart.ATOMIC挂起后")
+        }
+        atomicJob.cancel()
+        val undispatchedJob = GlobalScope.launch(start = CoroutineStart.UNDISPATCHED) {
+            Log.d("LogTest", "${Thread.currentThread()}CoroutineStart.UNDISPATCHED挂起前")
+            delay(100)
+            Log.d("LogTest", "CoroutineStart.UNDISPATCHED挂起后")
+        }
+        undispatchedJob.cancel()
     }
 
 
